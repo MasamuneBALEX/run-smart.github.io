ed5ca823f31dbd88d2bb75a9add23c48b8a0e771
MasamuneBALEX <107581562+MasamuneBALEX@users.noreply.github.com>
2024-12-05 00:24:39 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -181,4 +181,37 @@ $(document).ready(function () {
         });
         return false;
     });
+
+    // smooth scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.up-arrow').fadeIn();
+        } else {
+            $('.up-arrow').fadeOut();
+        }
+    });
+
+    $(".up-arrow").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 100, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    new WOW().init();
 });
